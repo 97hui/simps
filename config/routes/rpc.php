01b57136e7424458aa3643e040cdd900bf44cbad
97hui <zhouguohui1029@163.com>
2020-08-26 15:04:04 +0800

    Update rpc.php

--- config/routes/rpc.php
+++ config/routes/rpc.php
@@ -9,10 +9,4 @@ declare(strict_types=1);
  * @license  https://github.com/simple-swoole/simps/blob/master/LICENSE
  */
 return [
-    ['POST', '/api/resume/fileUpload', '\App\Controllers\ResumeController@fileUpload'],
-    ['POST', '/api/resume/save', '\App\Controllers\ResumeController@save'],
-    ['POST', '/api/resume/search', '\App\Controllers\ResumeController@search'],
-    ['POST', '/api/resume/detail', '\App\Controllers\ResumeController@detail'],
-    ['POST', '/api/erobot/saveDistributePositionInfo', '\App\Controllers\ERobotController@saveDistributePositionInfo'],
-    ['POST', '/api/erobot/listChannel', '\App\Controllers\ERobotController@listChannel'],
 ];
